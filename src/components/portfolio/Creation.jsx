7a27f8941f2f0bbd068a74a3b9014173b6bb6fed
Sulaimon Omotosho <susudef2u@gmail.com>
2023-11-16 11:35:37 +0100

    Finsihed portfolio UI with full responsiveness.

---
+++ src/components/portfolio/Creation.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Creation = () => {
+  return (
+    <div className='creation'>
+      <div className='desc'>
+        <h2>
+          full of energy, drive, <br /> and passion , our team aims to deliver
+          the most outstanding work in ever detail
+        </h2>
+        <p>
+          we believe that value-creation is the primary aim of any business and
+          the execution excellence is the basis of value-creation for
+          organizations and individuals
+        </p>
+        <button>contact us now</button>
+      </div>
+      <div className='image'>
+        <img src='./branding.jpg' alt='' />
+      </div>
+    </div>
+  )
+}
+
+export default Creation
---
+++ src/components/portfolio/Creative.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Creative = () => {
+  return (
+    <div className='creative'>
+      <div className='image'>
+        <div className='bg'>
+          <div className='imageContainer'>
+            <img src='./portfolio.PNG' alt='' />
+          </div>
+        </div>
+      </div>
+      <div className='desc'>
+        <h2>
+          we are creative people <br /> with big dreams
+        </h2>
+        <p>
+          the ability of our digital agency lies in an ever-evolving set of core
+          competencies nimbly combined and refined into a custom solution for
+          our clients and their brands
+        </p>
+        <button>contact us now</button>
+      </div>
+    </div>
+  )
+}
+
+export default Creative
